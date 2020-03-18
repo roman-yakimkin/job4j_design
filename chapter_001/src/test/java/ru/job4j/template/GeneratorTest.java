8986ca4f6c5c30832b8111ec53a5703053715b59
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-03-18 11:18:09 +0300

    Исправить код класс SimpleGenerator.[#251210]

---
+++ chapter_001/src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.template;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class GeneratorTest {
+
+    @Test @Ignore
+    public void produceCorrect() {
+        Generator generator = new TemplateGenerator();
+        String template = "I am ${name}, who are ${subject}?";
+        Map<String, Object> args = Map.of(
+                "name", "John",
+                "subject", "you"
+        );
+        String result = generator.produce(template, args);
+        String expected = "I am John, who are you?";
+        assertThat(result, is(expected));
+    }
+
+    @Test(expected = SuperfluousInTemplateException.class) @Ignore
+    public void superfluousInTemplate() throws SuperfluousInTemplateException{
+        Generator generator = new TemplateGenerator();
+        String template = "I am ${name}, my surname is ${surname}, who are ${subject}?";
+        Map<String, Object> args = Map.of(
+                "name", "John",
+                "subject", "you"
+        );
+        generator.produce(template, args);
+    }
+
+    @Test(expected = SuperfluousInArgsException.class) @Ignore
+    public void superfluousInTemplate() throws SuperfluousInArgsException{
+        Generator generator = new TemplateGenerator();
+        String template = "I am ${name}, who are ${subject}?";
+        Map<String, Object> args = Map.of(
+                "name", "John",
+                "surname", "Lennon",
+                "subject", "you"
+        );
+        generator.produce(template, args);
+    }
+}
