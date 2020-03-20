d15fb89b6290ef379088a2ce93e8d8bed9519578
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-03-20 10:45:23 +0300

    Исправить код класс SimpleGenerator.[#251210]

---
+++ chapter_001/src/main/java/ru/job4j/template/TemplateGenerator.java
@@ -0,0 +1,16 @@
+package ru.job4j.template;
+
+import java.util.Map;
+
+/**
+ * Класс для генерации шаблонов
+ * @author Roman Yakimkin (r.yakimkin@yandex.ru)
+ * @version 1.0
+ * @since 20.03.2020
+ */
+public class TemplateGenerator implements Generator {
+    @Override
+    public String produce(String template, Map<String, Object> args) {
+        return null;
+    }
+}
