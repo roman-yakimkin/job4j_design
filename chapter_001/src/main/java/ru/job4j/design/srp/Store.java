73ba6a54273e85f583e2c2d5d218f37e5dd8aca3
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-04-04 12:22:08 +0300

    На базе задания из занятия SRP расширить калькулятор[#251197]

--- chapter_001/src/main/java/ru/job4j/design/srp/Store.java
+++ chapter_001/src/main/java/ru/job4j/design/srp/Store.java
@@ -3,6 +3,12 @@ package ru.job4j.design.srp;
 import java.util.List;
 import java.util.function.Predicate;
 
+/**
+ * Интерфейс для хранилища данных
+ * @author Roman Yakimkin (r.yakimkin@yandex.ru)
+ * @since 04.04.2020
+ * @version 1.0
+ */
 public interface Store {
         List<Employee> findBy(Predicate<Employee> filter);
 }
