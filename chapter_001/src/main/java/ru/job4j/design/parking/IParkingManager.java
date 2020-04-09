0acd9a760970c5c882e2a2f6951bc30a123b41f7
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-04-09 11:11:01 +0300

    Парковка машин[#251201]. Перенес файлы в правильную директорию

---
+++ chapter_001/src/main/java/ru/job4j/design/parking/IParkingManager.java
@@ -0,0 +1,12 @@
+package ru.job4j.design.parking;
+
+/**
+ * Интерфейс - менеджер парковки
+ * @author Roman Yakimkin (r.yakimkin@yandex.ru)
+ * @since 07.04.2020
+ * @version 1.0
+ */
+public interface IParkingManager {
+    public IParking getParking();
+    
+}
