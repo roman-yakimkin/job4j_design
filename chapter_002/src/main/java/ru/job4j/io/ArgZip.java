8baad1d44ebfeb7f5398bfb8bf9c4587ddb84046
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-04-27 21:22:50 +0300

    Архивировать проект. [#251177]

--- chapter_002/src/main/java/ru/job4j/io/ArgZip.java
+++ chapter_002/src/main/java/ru/job4j/io/ArgZip.java
@@ -1,5 +1,13 @@
 package ru.job4j.io;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Класс для работы с аргументами архива
  * @author Roman Yakimkin (r.yakimkin@yandex.ru)
@@ -9,23 +17,57 @@ package ru.job4j.io;
 public class ArgZip {
     private final String[] args;
 
+    private String directory;
+    private String exclude = "";
+    private String output;
+    private final boolean lastParsingResult;
+
     public ArgZip(String[] args) {
         this.args = args;
+        lastParsingResult = tryToParse();
+    }
+
+    private boolean tryToParse() {
+        List<String> collectArgs = Arrays.asList(args);
+        int indexDir = collectArgs.indexOf("-d");
+        if (indexDir == -1 || indexDir == collectArgs.size() - 1) {
+            return false;
+        } else {
+            Path dir = Paths.get(collectArgs.get(indexDir + 1));
+            if (!Files.exists(dir) || !Files.isDirectory(dir)) {
+                return false;
+            }
+            directory = collectArgs.get(indexDir + 1);
+        }
+
+        int indexOutput = collectArgs.indexOf("-o");
+        if (indexOutput == -1 || indexOutput == collectArgs.size() - 1) {
+            return false;
+        } else {
+            Path oFile = Paths.get(collectArgs.get(indexOutput + 1));
+            output = collectArgs.get(indexOutput + 1);
+        }
+
+        int indexExclude = collectArgs.indexOf("-e");
+        if (indexExclude != -1 || indexExclude != collectArgs.size() - 1) {
+            exclude = collectArgs.get(indexExclude + 1);
+        }
+        return true;
     }
 
     public boolean valid() {
-        return false;
+        return lastParsingResult;
     }
 
     public String directory() {
-        return null;
+        return directory;
     }
 
     public String exclude() {
-        return null;
+        return exclude;
     }
 
     public String output() {
-        return null;
+        return output;
     }
 }
