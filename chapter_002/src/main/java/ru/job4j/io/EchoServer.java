b7112ce5ec59e4c73b0c602a7a14230bd423adcb
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-04-29 12:35:55 +0300

    Бот[#251187]

--- chapter_002/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_002/src/main/java/ru/job4j/io/EchoServer.java
@@ -6,6 +6,8 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Класс - сокет-сервер
@@ -15,20 +17,35 @@ import java.net.Socket;
  */
 public class EchoServer {
     public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("\\?msg=.+ HTTP");
         try (ServerSocket server = new ServerSocket(9000)) {
             boolean isRunning = true;
             while (isRunning) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-                    String str;
+                    String str, answerText = "";
                     while (!(str = in.readLine()).isEmpty()) {
-                        System.out.println(str);
-                        if (str.contains(" msg=Bye ")) {
-                            isRunning = false;
+                        Matcher matcher = pattern.matcher(str);
+                        if (matcher.find()) {
+                            String msg = str.substring(matcher.start() + 5, matcher.end() - 5);
+                            answerText = msg;
+                            switch (msg.toLowerCase()) {
+                                case "hello" :
+                                    answerText = "Hello, dear friend !";
+                                    break;
+                                case "exit" :
+                                    answerText = "See you soon ...";
+                                    isRunning = false;
+                                    break;
+                                default:
+                                    break;
+                            }
                         }
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write(("<html><body><div style=\"position:absolute;width:100%;top:50%;text-align:center;font-size:40px;\">" + answerText + "</div></body></html>").getBytes());
+                    out.flush();
                 }
             }
         } catch (IOException e) {
