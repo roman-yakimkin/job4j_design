c7bcc898ff21fea4a9c53e37f6a78a45fbdb5182
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-05-06 10:32:50 +0300

    Видео[#251147]

---
+++ chapter_003/src/main/java/ru/job4j/it/BackwardArrayIt.java
@@ -0,0 +1,32 @@
+package ru.job4j.it;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Итератор для массива, отдающий элементы в обратном порядке
+ * @author Roman Yakimkin (r.yakimkin@yandex.ru)
+ * @since 06.05.2020
+ * @version 1.0
+ */
+public class BackwardArrayIt implements Iterator<Integer> {
+    private final int[] data;
+    private int point = 0;
+
+    public BackwardArrayIt(int[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return point < data.length;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[data.length - point++ - 1];
+    }
+}
