f1db861e163bd0b01e9d3c2c2712cb7bb65d9db8
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-05-09 17:04:16 +0300

    Создать модель User[#251157]

---
+++ chapter_003/src/main/java/ru/job4j/collection/User.java
@@ -0,0 +1,33 @@
+package ru.job4j.collection;
+
+import java.util.Calendar;
+
+/**
+ * Реализация модели User
+ * @author Roman Yakimkin (r.yakimkin@yandex.ru)
+ * @since 09.05.2020
+ * @version 1.0
+ */
+public class User {
+    private String name;
+    private int children = 0;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+}
