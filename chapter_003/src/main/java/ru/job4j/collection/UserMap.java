45e15c0c236a33207ac235de2d5730022fde82ba
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-05-10 13:30:56 +0300

    Не перекрывать equals hashCode[#251161]

---
+++ chapter_003/src/main/java/ru/job4j/collection/UserMap.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Класс для экспериментов с картой пользователей
+ * @author Roman Yakimkin (r.yakimkin@yandex.ru)
+ * @since 10.05.2020
+ * @version 1.0
+ */
+public class UserMap {
+    public static void main(String[] args) {
+        User first = new User("Иванов Петр Сергеевич", 2, new GregorianCalendar(1980, 1, 10));
+        User second = new User("Иванов Петр Сергеевич", 2, new GregorianCalendar(1980, 1, 10));
+        Map<User, Object> users = new HashMap<>();
+        users.put(first, new Object());
+        users.put(second, new Object());
+        System.out.println(users.toString());
+    }
+}
