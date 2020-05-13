97b41e3017760b210b377d61db7b59fdfb4263e3
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-05-13 13:09:06 +0300

    Создать элементарную структуру дерева[#251168]

---
+++ chapter_003/src/main/java/ru/job4j/collection/SimpleTree.java
@@ -0,0 +1,26 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Интерфейс - реализация дерева
+ * @author Roman Yakimkin (r.yakimkin@yandex.ru)
+ * @since 13.05.2020
+ * @version 1.0
+ */
+public interface SimpleTree<E> {
+    boolean add(E parent, E child);
+
+    Optional<Node<E>> findBy(E value);
+
+    class Node<E> {
+        final E value;
+        final List<Node<E>> children = new ArrayList<>();
+
+        public Node(E value) {
+            this.value = value;
+        }
+    }
+}
