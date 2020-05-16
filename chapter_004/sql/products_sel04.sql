8a543bfcd762f51701229800e1060ef90783900e
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-05-16 14:11:10 +0300

    Фильтры[#251126]

---
+++ chapter_004/sql/products_sel04.sql
@@ -0,0 +1,14 @@
+﻿--Написать запрос, который выводит самый дорогой продукт.
+
+-- 1 способ
+
+select * 
+from products p
+order by price desc
+limit 1
+
+-- 2 способ
+
+select *
+from products p 
+where p.price = (select max(price) from products)
---
+++ chapter_004/sql/products_sel07.sql
@@ -0,0 +1,8 @@
+﻿--Написать запрос, который выводит тип продуктов, которых осталось меньше 10 штук.  
+
+select t.*
+from product_types t
+inner join products p
+on t.id = p.type_id
+group by t.id
+having count(p.type_id) < 10
