db553bd5259087114b02fbf0eb0bd975acb388f2
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-05-18 13:22:28 +0300

    XML XSLT JDBC Оптимизация[#251108]

---
+++ chapter_004/src/main/java/ru/job4j/spammer/ImportDB.java
@@ -0,0 +1,78 @@
+package ru.job4j.spammer;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * Класс для импорта таблицы спаммеров из текстового файла
+ * @author Roman Yakimkin (r.yakimkin@yandex.ru)
+ * @since 18.05.2020
+ * @version 1.0
+ */
+public class ImportDB {
+    private Properties cfg;
+    private String dump;
+
+    public ImportDB(Properties cfg, String dump) {
+        this.cfg = cfg;
+        this.dump = dump;
+    }
+
+    public List<User> load() throws IOException {
+        List<User> users = new ArrayList<>();
+        try (BufferedReader rd = new BufferedReader(new FileReader(dump))) {
+            rd.lines().forEach((line) -> {
+                String[] content = line.split(";");
+                users.add(new User(content[0], content[1]));
+            });
+        }
+        return users;
+    }
+
+    public void save(List<User> users) throws ClassNotFoundException, SQLException {
+        Class.forName(cfg.getProperty("jdbc.driver"));
+        try (Connection cnt = DriverManager.getConnection(
+                cfg.getProperty("jdbc.url"),
+                cfg.getProperty("jdbc.username"),
+                cfg.getProperty("jdbc.password")
+        )) {
+            for (User user : users) {
+                try (PreparedStatement ps = cnt.prepareStatement("insert into users (name, email) values (?, ?)")) {
+                    ps.setString(1, user.name);
+                    ps.setString(2, user.email);
+                    ps.execute();
+                }
+            }
+        }
+    }
+
+    private static class User {
+        String name;
+        String email;
+
+        public User(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Properties cfg = new Properties();
+        try (FileInputStream in = new FileInputStream("./chapter_004/src/main/resources/spammer.properties")) {
+            cfg.load(in);
+        }
+        ImportDB db = new ImportDB(cfg, "./chapter_004/src/main/resources/dump.txt");
+        db.save(db.load());
+    }
+}
