4aefc4fb1a82f1d4f87a0edc10ec838472cceda2
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-05-19 09:47:56 +0300

    Хранилище машин[#251129]

--- chapter_004/sql/cars_all_cars.sql
+++ chapter_004/sql/cars_all_cars.sql
@@ -2,6 +2,6 @@
 
 select c.name, cb.name as car_body, ct.name as transmission, ce.name as engine  
 from cars c
-inner join car_bodies cb on c.body_id = cb.id
-inner join car_engines ce on c.engine_id = ce.id
-inner join car_transmissions ct on c.transmission_id = ct.id
+left join car_bodies cb on c.body_id = cb.id
+left join car_engines ce on c.engine_id = ce.id
+left join car_transmissions ct on c.transmission_id = ct.id
