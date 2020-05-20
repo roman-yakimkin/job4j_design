cb014ddc5abf628820546dcec7144c8e5fb558d0
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-05-20 19:31:45 +0300

    Парсер вакансий на sql.ru[1731#251119]

--- chapter_004/sql/vacancies.sql
+++ chapter_004/sql/vacancies.sql
@@ -1,4 +1,5 @@
-﻿DROP SCHEMA IF EXISTS post;
+﻿DROP TABLE IF EXISTS post.vacancies;
+DROP SCHEMA IF EXISTS post;
 
 CREATE SCHEMA post;
 
@@ -6,6 +7,6 @@ CREATE TABLE post.vacancies (
 	id serial primary key,
 	name varchar(2000) not null,
 	text text not null,
-	link varchar(500) not null,
+	link varchar(500) unique not null,
 	created timestamp not null	
 );
