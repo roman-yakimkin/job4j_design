04fd4c064e22b851cb55fc2fd6efe822f0cc5ebc
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-05-21 14:57:31 +0300

    Select by company[2760#251120]

---
+++ chapter_004/sql/companies.sql
@@ -0,0 +1,31 @@
+﻿drop table if exists company, person;
+
+create table company (
+	id integer not null,
+	name varchar,
+	constraint company_pkey primary key(id)
+);
+
+create table person (
+	id integer not null,
+	name varchar,
+	company_id integer references company(id),
+	constraint person_pkey primary key(id)	
+);
+
+insert into company (id, name) values
+	(1, 'Microsoft'),
+	(2, 'Apple'),
+	(3, 'Borland'),
+	(4, 'JetBrains'),
+	(5, 'Gazprom'),
+	(6, 'The horns and hooves inc.');
+
+insert into person (id, name, company_id) values 
+	(1, 'Gates', 1),
+	(2, 'Johnson', 3),
+	(3, 'Peterson', 2),
+	(4, 'Sidorov', 4),
+	(5, 'Potapov', 5),
+	(6, 'Smirnov', 5),
+	(7, 'Balaganov', 6);
---
+++ chapter_004/sql/companies_sel2.sql
@@ -0,0 +1,40 @@
+﻿-- Select the name of the company with the maximum number of persons + number of persons in this company
+
+-- способ 1
+select c.name, count(p.id) as staff
+from company c 
+inner join person p 
+on c.id = p.company_id
+group by c.id 
+order by staff desc
+limit 1
+
+-- способ 2
+select c.name, count(p.id) as staff
+from company c inner join person p on c.id = p.company_id
+group by c.id
+having count(p.id) = (select max(v.cnt1) from (
+	select count(p1.id) as cnt1 
+	from company c1 
+	inner join person p1 
+	on c1.id = p1.company_id 
+	group by c1.id 
+	) v)
+
+-- способ 3
+select v1.name, v1.cnt1 as staff
+from (
+	select c1.name, count(p1.id) as cnt1 
+	from company c1 
+	inner join person p1 
+	on c1.id = p1.company_id 
+	group by c1.id 		
+) v1
+where v1.cnt1 = (select max(v2.cnt1)
+		 from (
+			select c1.name, count(p1.id) as cnt1 
+			from company c1 
+			inner join person p1 
+			on c1.id = p1.company_id 
+			group by c1.id 
+		      ) v2)
