b910a687e83c9d3f3905ae8dc43d6e1391714de0
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-05-25 21:25:01 +0300

    SQL

---
+++ chapter_004/sql/meetings_sel1.sql
@@ -0,0 +1,11 @@
+﻿--Нужно написать запрос, который получит список всех заяков и количество подтвердивших участников
+
+select m.name, count(mu.user_id)
+from m_meetings m
+left join (
+	select * 
+	from m_meet_users 
+	where state_id = 1
+	) mu
+on m.id = mu.meeting_id
+group by m.id
---
+++ chapter_004/sql/meetings_sel2.sql
@@ -0,0 +1,8 @@
+﻿-- Нужно получить все совещания, где не было ни одной заявки на посещения
+
+select * 
+from m_meetings
+where id not in (
+	select meeting_id 
+	from m_meet_users
+	)
