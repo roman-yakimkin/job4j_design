f618fc105ad55139dd73238672b08a73a79163d6
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-06-04 10:08:47 +0300

    Реализовать шаблон Producer Consumer.[1098#297382]

---
+++ chapter_007/src/main/java/ru/job4j/synch/SimpleBlockingQueue.java
@@ -0,0 +1,40 @@
+package ru.job4j.synch;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Класс - блокирующая очередь
+ * @author Roman Yakimkin (r.yakimkin@yandex.ru)
+ * @since 04.06.2020
+ * @version 1.0
+ */
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+    @GuardedBy("this")
+    private final Queue<T> queue = new LinkedList<>();
+
+    public synchronized void offer(T value) {
+        queue.offer(value);
+        notifyAll();
+    }
+
+    public synchronized T poll() {
+        while (queue.isEmpty()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        }
+        return queue.poll();
+    }
+
+    public synchronized boolean isEmpty() {
+        return queue.isEmpty();
+    }
+}
---
+++ chapter_007/src/test/java/ru/job4j/synch/SimpleBlockingQueueTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.synch;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class SimpleBlockingQueueTest {
+    @Test
+    public void testSimpleBlockingQueueWithOneProducerAndOneConsumer() throws InterruptedException {
+        List<Integer> output = new ArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        Thread producer = new Thread(
+                () -> {
+                    for (int index = 0; index < 10; index++) {
+                       queue.offer(index);
+                    }
+                }
+        );
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                      output.add(queue.poll());
+                    }
+                }
+        );
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+
+        assertThat(output, is(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)));
+
+    }
+}
